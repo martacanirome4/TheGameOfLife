44d34e3bba7ecc402b4fde75acde0c63e7e83400
MartaCanino <martacaninoromero@gmail.com>
2023-03-10 09:51:51 +0100

    Crear esqueleto inicial clases Tablero y Principal (de enunciado)

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,38 @@
+package aplicacion;
+
+import dominio.Tablero;
+
+import java.util.concirrent.TimeUnit;
+import java.lang.InterruptedException;
+
+public class Principal {
+    public static viod main (String[] args) {
+        try
+        {
+            Tablero tabler = new Tablero();
+            System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
+            tablero.leerEstadoActual();
+            System.out.println(tablero);
+            for(int i = 0; i<= 5; i++)
+            {
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+            }
+
+            System.out.println("SIMULACIÓN CON TABLERO CREADO MEDIANTE MONTECARLO");
+            tablero.generarEstadoActualPorMontecarlo();
+            System.out.println(tablero);
+            for(int i = 0; i<= 15; i++)
+            {
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/dominio/Tablero.java
@@ -0,0 +1,44 @@
+package dominio;
+// Tablero de El Juego de la Vida
+
+public class Tablero{
+    private static int DIMENSION = 30;
+    // matriz que representa el estado actual del tablero
+    private int[][] estadoActual;
+    // matriz que representa el estado siguiente del tablero
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+    // lee el estado inicial de un fichero llamado 'matriz'
+    public void leerEstadoActtual(){
+        // ...
+    }
+
+    /* * Genera un estado inicial aleatorio. Para cada celda
+          * genera un número aleatorio en el intervalo [0, 1). Si
+          * el número es menor que 0,5, entonces la celda está
+          * inicialmente viva. En caso contrario, está muerta. */
+    public void generarEstadoActualPorMontecarlo(){
+        // La secuencia de ceros y unos generada es guardada
+        // en ‘estadoActual‘ y, utilizando las reglas del juego
+        // de la vida, se insertan los ceros y unos
+        // correspondientes en ‘estadoSiguiente‘.
+    }
+
+
+    /** Transita al estado siguiente según las reglas del
+     * juego de la vida. */
+    public void transitarAlEstadoSiguiente(){
+        // La variable ‘estadoActual‘ pasa a tener el contenido
+        // de ‘estadoSiguiente‘ y, éste útimo atributo pasar a
+        // reflejar el siguiente estado.
+    }
+
+    /* * Devuelve, en modo texto, el estado actual.
+          * @return el estado actual. */
+    @Override
+    public String toString(){
+        return ""; // modificar
+    }
+
+
+}
