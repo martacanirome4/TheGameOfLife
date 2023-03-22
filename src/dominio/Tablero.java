ae76c15f4ffbaef369e5825aee1812d67b4beced
MartaCanino <martacaninoromero@gmail.com>
2023-03-22 13:29:34 +0100

    Corregir errores menores en variables y comentarios

--- src/dominio/Tablero.java
+++ src/dominio/Tablero.java
@@ -2,120 +2,136 @@ package dominio;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-// Tablero de El Juego de la Vida
 
+/**
+ * Esta clase es responsable de leer el tablero de un
+ * fichero en forma de ceros y unos, ir transitando de
+ * estados e ir mostrando dichos estados.
+ */
 public class Tablero {
-    private static int DIMENSION = 30;
-    // matriz que representa el estado actual del tablero
-    private int[][] estadoActual = new int[DIMENSION+2][DIMENSION+2];
-    // matriz que representa el estado siguiente del tablero
-    // le indicamos que es 30x30 pero realmente es 32x32 para no salirnos del tablero al recorrer y calcular en límites
-    private int[][] estadoSiguiente = new int[DIMENSION+2][DIMENSION+2];
+    // Pasaremos el tamaño de dimension del tablero como 30x30
+    // Realmente el archivo será de 32x32 para evitar problemas con los bordes, por lo que hacemos dimension+2
+    private static int dimension = 30;
+    private int[][] estado_actual = new int[dimension+2][dimension+2];
+    private int[][] estado_siguiente = new int[dimension+2][dimension+2];
 
-    // lee el estado inicial de un fichero llamado 'matriz'
+    /**
+     * Lee el estado inicial del fichero "matriz.txt"
+     * La secuencia de ceros y unos del fichero es guardada en ‘estadoActual‘ y, utilizando las reglas del juego
+     * de la vida, se insertan los ceros y unos correspondientes en ‘estadoSiguiente‘
+     */
     public void leerEstadoActual(){
         try {
-            Scanner fichero = new Scanner(new File("matriz.txt"));
-            for (int i = 0; i < DIMENSION; i++) {
-                String linea = fichero.nextLine();
-                for (int j = 0; j < DIMENSION; j++) {
-                    estadoActual[i+1][j+1] = Integer.parseInt(String.valueOf(linea.charAt(j)));
+            Scanner file = new Scanner(new File("matriz.txt"));
+            for (int i=0; i<dimension; i++) {
+                String linea = file.nextLine();
+                for (int j=0; j<dimension; j++) {
+                    estado_actual[i+1][j+1] = Integer.parseInt(String.valueOf(linea.charAt(j)));
                 }
             }
 
-            for (int i = 1; i < DIMENSION+1; i++) {
-                for (int j = 1; j < DIMENSION+1; j++) {
-                    int vecinasVivas = estadoActual[i - 1][j - 1] + estadoActual[i - 1][j] + estadoActual[i - 1][j + 1]
-                            + estadoActual[i][j - 1] + estadoActual[i][j + 1] + estadoActual[i + 1][j - 1] +
-                            estadoActual[i + 1][j] + estadoActual[i + 1][j + 1];
-                    if (estadoActual[i][j] == 1 && (vecinasVivas == 2 || vecinasVivas == 3)) {
-                        estadoSiguiente[i][j] = 1;
-                    } else if (estadoActual[i][j] == 0 && vecinasVivas == 3) {
-                        estadoSiguiente[i][j] = 1;
+            /** Genera un estado inicial aleatorio. Para cada celda
+             * genera un número aleatorio en el intervalo [0, 1). Si
+             * el número es menor que 0,5, entonces la celda está
+             * inicialmente viva. En caso contrario, está muerta. */
+            for (int i=1; i<dimension+1; i++) {
+                for (int j=1; j<dimension+1; j++) {
+                    // Recorremos las 8 casillas que rodean a la actual, o sus 'vecinas'
+                    int vecinas_vivas = estado_actual[i-1][j-1]
+                                        + estado_actual[i-1][j]
+                                        + estado_actual[i-1][j+1]
+                                        + estado_actual[i][j-1]
+                                        + estado_actual[i][j+1]
+                                        + estado_actual[i+1][j-1]
+                                        + estado_actual[i+1][j]
+                                        + estado_actual[i+1][j+1];
+                    if (estado_actual[i][j]==1 && (vecinas_vivas==2 || vecinas_vivas==3)) {
+                        estado_siguiente[i][j] = 1;
+                    } else if (estado_actual[i][j]==0 && vecinas_vivas==3) {
+                        estado_siguiente[i][j] = 1;
                     } else {
-                        estadoSiguiente[i][j] = 0;
+                        estado_siguiente[i][j] = 0;
                     }
                 }
             }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    /* * Genera un estado inicial aleatorio. Para cada celda
-          * genera un número aleatorio en el intervalo [0, 1). Si
-          * el número es menor que 0,5, entonces la celda está
-          * inicialmente viva. En caso contrario, está muerta. */
+    /** La secuencia de ceros y unos generada es guardada en ‘estado_actual‘ y,
+     * utilizando las reglas del juego de la vida,
+     * se insertan los ceros y unos correspondientes en ‘estadoSiguiente‘.
+     * Transita al estado siguiente según las reglas del juego.
+     * **/
     public void generarEstadoActualPorMontecarlo(){
-        // La secuencia de ceros y unos generada es guardada
-        // en ‘estadoActual‘ y, utilizando las reglas del juego
-        // de la vida, se insertan los ceros y unos
-        // correspondientes en ‘estadoSiguiente‘.
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                estadoActual[i+1][j+1] = (int) Math.round(Math.random());
+
+        for (int i=0; i<dimension; i++) {
+            for (int j=0; j<dimension; j++) {
+                estado_actual[i+1][j+1] = (int) Math.round(Math.random());
             }
         }
-
-        for (int i = 1; i < DIMENSION+1; i++) {
-            for (int j = 1; j < DIMENSION+1; j++) {
-                int vecinasVivas = estadoActual[i - 1][j - 1] + estadoActual[i - 1][j] + estadoActual[i - 1][j + 1]
-                        + estadoActual[i][j - 1] + estadoActual[i][j + 1] + estadoActual[i + 1][j - 1] +
-                        estadoActual[i + 1][j] + estadoActual[i + 1][j + 1];
-                if (estadoActual[i][j] == 1 && (vecinasVivas == 2 || vecinasVivas == 3)) {
-                    estadoSiguiente[i][j] = 1;
-                } else if (estadoActual[i][j] == 0 && vecinasVivas == 3) {
-                    estadoSiguiente[i][j] = 1;
+        for (int i = 1; i<dimension+1; i++) {
+            for (int j=1; j<dimension+1; j++) {
+                int vecinas_vivas = estado_actual[i-1][j-1]
+                                    + estado_actual[i-1][j]
+                                    + estado_actual[i-1][j+1]
+                                    + estado_actual[i][j-1]
+                                    + estado_actual[i][j+1]
+                                    + estado_actual[i+1][j-1]
+                                    + estado_actual[i+1][j]
+                                    + estado_actual[i+1][j+1];
+                if (estado_actual[i][j]==1 && (vecinas_vivas==2 || vecinas_vivas==3)) {
+                    estado_siguiente[i][j] = 1;
+                } else if (estado_actual[i][j]==0 && vecinas_vivas==3) {
+                    estado_siguiente[i][j] = 1;
                 } else {
-                    estadoSiguiente[i][j] = 0;
+                    estado_siguiente[i][j] = 0;
                 }
             }
         }
     }
 
 
-    /** Transita al estado siguiente según las reglas del
-     * juego de la vida. */
+    /** La variable ‘estado_actual‘ pasa a tener el contenido de ‘estadoSiguiente‘ y,
+     * éste útimo atributo pasar a reflejar el siguiente estado.**/
     public void transitarAlEstadoSiguiente(){
-        // La variable ‘estadoActual‘ pasa a tener el contenido
-        // de ‘estadoSiguiente‘ y, éste útimo atributo pasar a
-        // reflejar el siguiente estado.
-        estadoActual = estadoSiguiente;
-        estadoSiguiente = new int[DIMENSION+2][DIMENSION+2];
-        for (int i = 1; i < DIMENSION+1; i++) {
-            for (int j = 1; j < DIMENSION+1; j++) {
-                int vecinasVivas = estadoActual[i - 1][j - 1] + estadoActual[i - 1][j] + estadoActual[i - 1][j + 1]
-                        + estadoActual[i][j - 1] + estadoActual[i][j + 1] + estadoActual[i + 1][j - 1] +
-                        estadoActual[i + 1][j] + estadoActual[i + 1][j + 1];
-                if (estadoActual[i][j] == 1 && (vecinasVivas == 2 || vecinasVivas == 3)) {
-                    estadoSiguiente[i][j] = 1;
-                } else if (estadoActual[i][j] == 0 && vecinasVivas == 3) {
-                    estadoSiguiente[i][j] = 1;
+
+        estado_actual = estado_siguiente;
+        estado_siguiente = new int[dimension+2][dimension+2];
+        for (int i=1; i<dimension+1; i++) {
+            for (int j=1; j<dimension+1; j++) {
+                int vecinas_vivas = estado_actual[i-1][j-1]
+                                    + estado_actual[i-1][j]
+                                    + estado_actual[i-1][j+1]
+                                    + estado_actual[i][j-1]
+                                    + estado_actual[i][j+1]
+                                    + estado_actual[i+1][j-1]
+                                    + estado_actual[i+1][j]
+                                    + estado_actual[i+1][j+1];
+                if (estado_actual[i][j]==1 && (vecinas_vivas==2 || vecinas_vivas==3)) {
+                    estado_siguiente[i][j] = 1;
+                } else if (estado_actual[i][j]==0 && vecinas_vivas==3) {
+                    estado_siguiente[i][j] = 1;
                 } else {
-                    estadoSiguiente[i][j] = 0;
+                    estado_siguiente[i][j] = 0;
                 }
             }
         }
     }
 
-    /* * Devuelve, en modo texto, el estado actual.
-          * @return el estado actual. */
     @Override
     public String toString(){
 
-        StringBuilder cadena = new StringBuilder();
-        for (int i = 1; i < DIMENSION+1; i++) {
-            for (int j = 1; j < DIMENSION+1; j++) {
-                if (estadoActual[i][j] == 0) {
-                    cadena.append(" ");
-                } else {
-                    cadena.append("x");
-                }
+        StringBuilder resultado = new StringBuilder();
+        for (int i=1; i<dimension+1; i++) {
+            for (int j=1; j<dimension+1; j++) {
+                if (estado_actual[i][j] == 0) { resultado.append(" ");}
+                else { resultado.append("x");}
             }
-            cadena.append("\n");
+            resultado.append("\n");
         }
-        return cadena.toString();
+        return resultado.toString();
     }
 
 }
